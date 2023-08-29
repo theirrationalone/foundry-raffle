fa71576363b1ffe9ad0df326f075f56f50b5e6ea
theirrationalone <anilkushwaha289@gmail.com>
2023-08-30 02:25:35 +0530

    DeployRaffle Test Performed 🧪

--- test/unit/DeployRaffleTest.t.sol
+++ test/unit/DeployRaffleTest.t.sol
@@ -6,7 +6,21 @@ import {Test} from "forge-std/Test.sol";
 
 import {Raffle} from "../../src/Raffle.sol";
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract DeployRaffleTest is Test {
-    function setUp() external {}
+    DeployRaffle deployer;
+
+    function setUp() external {
+        deployer = new DeployRaffle();
+    }
+
+    function testDeployRaffleScriptWorksCorrectly() public {
+        (Raffle raffle, HelperConfig config, uint64 subId) = deployer.run();
+
+        assert(address(deployer) != address(0));
+        assert(address(raffle) != address(0));
+        assert(address(config) != address(0));
+        assert(subId != 0);
+    }
 }
---
+++ test/unit/LinkTokenTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+
+import {LinkToken} from "../mocks/LinkToken.m.sol";
+
+contract LinkTokenTest is Test {
+    function setUp() external {}
+}
