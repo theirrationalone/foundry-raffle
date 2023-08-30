a4f2245a066670c0c22257dfc4a4c7a8815d21e6
theirrationalone <anilkushwaha289@gmail.com>
2023-08-30 15:20:32 +0530

    Fuzz Invariant Testing Performed 🧪⚡

--- test/Fuzz/RaffleHandler.t.sol
+++ test/Fuzz/RaffleHandler.t.sol
@@ -2,14 +2,65 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 import {Raffle} from "../../src/Raffle.sol";
 
 contract RaffleHandler is Test {
     Raffle private immutable i_raffle;
+    VRFCoordinatorV2Mock private immutable i_vrfCoordinator;
+    address private immutable i_vrfCoordinatorV2Address;
+    address private immutable i_linkTokenAddress;
+    uint64 private immutable i_subId;
 
-    constructor(address _raffleAddress) {
-        i_raffle = Raffle(_raffleAddress);
+    constructor(address _raffleAddress, address _vrfCoordinatorV2Address, uint64 _subId, address _linkTokenAddress) {
+        i_raffle = Raffle(payable(_raffleAddress));
+        i_vrfCoordinatorV2Address = _vrfCoordinatorV2Address;
+        i_linkTokenAddress = _linkTokenAddress;
+        i_vrfCoordinator = VRFCoordinatorV2Mock(_vrfCoordinatorV2Address);
+        i_subId = _subId;
+    }
+
+    function perforUpkeep(address _entrantSeed) public {
+        uint256 entranceFee = 0.001 ether;
+
+        if (i_raffle.getRaffleState() != 0) {
+            return;
+        }
+
+        if (
+            _entrantSeed == address(0) || _entrantSeed == i_vrfCoordinatorV2Address
+                || _entrantSeed == i_linkTokenAddress
+        ) {
+            return;
+        }
+
+        hoax(_entrantSeed, entranceFee);
+        i_raffle.enterRaffle{value: entranceFee}();
+
+        for (uint160 i = 1; i <= 10; i++) {
+            hoax(address(i), 10 ether);
+            i_raffle.enterRaffle{value: entranceFee}();
+        }
+
+        vm.warp(block.timestamp + 31);
+        vm.roll(block.number + 1);
+
+        (bool upkeepNeeded,) = i_raffle.checkUpkeep("0x0");
+
+        if (!upkeepNeeded) {
+            return;
+        }
+
+        vm.recordLogs();
+        i_raffle.performUpkeep("0x0");
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        bytes32 requestIdProto = entries[1].topics[1];
+
+        i_vrfCoordinator.fundSubscription(i_subId, 10 ether);
+        i_vrfCoordinator.fulfillRandomWords(uint256(requestIdProto), address(i_raffle));
     }
 }
--- test/Fuzz/RaffleInvariants.t.sol
+++ test/Fuzz/RaffleInvariants.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 
 import {Raffle} from "../../src/Raffle.sol";
@@ -17,10 +17,35 @@ contract RaffleInvariants is StdInvariant, Test {
         HelperConfig config = new HelperConfig();
 
         DeployRaffle deployer = new DeployRaffle();
-        (raffle, config,) = deployer.run();
+        uint64 subId;
+        (raffle, config, subId) = deployer.run();
 
-        RaffleHandler handler = new RaffleHandler(address(raffle));
+        (address vrfCoordinatorV2Address,,,,,,, address linkTokenAddress) = config.activeNetworkConfig();
+
+        RaffleHandler handler = new RaffleHandler(address(raffle), vrfCoordinatorV2Address, subId, linkTokenAddress);
 
         targetContract(address(handler));
+    }
+
+    function invariant_lotteryCanHaveOnlyOneWinnerInAnyWay() public {
+        address winner = raffle.getWinner();
+
+        assertEq(raffle.getRaffleState(), 0);
+        assert(winner.balance >= address(raffle).balance);
+    }
+
+    function invariant_helperFunctionsMustNotRevert() public view {
+        raffle.getCallbackGasLimit();
+        raffle.getEntranceFee();
+        raffle.getEntrantsLength();
+        raffle.getGasLane();
+        raffle.getInterval();
+        raffle.getLastTimestamp();
+        raffle.getMinimumRequestConfirmations();
+        raffle.getNumWords();
+        raffle.getRaffleState();
+        raffle.getSubId();
+        raffle.getVRFCoordinatorV2Address();
+        raffle.getWinner();
     }
 }
